1dfa09a33a535566d62d90ba25033a0f7eb375b2
robin kumar <robinkr.8cl@gmail.com>
2021-07-13 15:44:51 +0530

    .

--- storywory-backend/drfauth/drfauth/settings.py
+++ storywory-backend/drfauth/drfauth/settings.py
@@ -155,5 +155,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'storyworybot@gmail.com'
-EMAIL_HOST_PASSWORD = '12345robin'
+EMAIL_HOST_USER = '****'
+EMAIL_HOST_PASSWORD = '****'
